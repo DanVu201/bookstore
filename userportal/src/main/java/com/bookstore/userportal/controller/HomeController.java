60bb95a58dd5e6757ddb87fd91922945b624f2b0
Vu Duy Dan <vuduydan03072001@gmail.com>
2022-05-28 17:19:34 +0700

    edit tem pờ lết

--- userportal/src/main/java/com/bookstore/userportal/controller/HomeController.java
+++ userportal/src/main/java/com/bookstore/userportal/controller/HomeController.java
@@ -105,6 +105,14 @@ public class HomeController {
         }
 
         Book book = bookService.findById(id);
+        String image;
+        try {
+            image = Base64.getEncoder().encodeToString(book.getImageShow());
+        } catch (Exception ignored) {
+            image = "";
+        }
+        book.setImageString(image);
+
         model.addAttribute("book", book);
 
         List<Integer> qtyList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
